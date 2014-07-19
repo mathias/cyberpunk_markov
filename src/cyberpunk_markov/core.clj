d4beb594fde921f395d113d45c05a1b5022add7e
Matt Gauger <matt.gauger@gmail.com>
2014-07-19 13:30:15 -0500

    Some minor cleanup

--- src/cyberpunk_markov/core.clj
+++ src/cyberpunk_markov/core.clj
@@ -5,7 +5,7 @@
             [opennlp.nlp :refer :all]
             [opennlp.treebank :refer :all]))
 
-(def combined-books (atom ""))
+;;(def combined-books (atom ""))
 ;;(def neuromancer-map (atom {}))
 ;;(def accelerando-map (atom {}))
 
@@ -21,11 +21,11 @@
  [filename]
  (slurp (io/file (io/resource filename))))
 
-(defn load-books []
-  (reset! combined-books
-          (str (load-resource "accelerando.txt")
-               "\n\n"
-               (load-resource "neuromancer.txt"))))
+;; (defn load-books []
+;;   (reset! combined-books
+;;           (str (load-resource "accelerando.txt")
+;;                "\n\n"
+;;                (load-resource "neuromancer.txt"))))
 
 ;;(def accelerando (load-resource "accelerando.txt"))
 ;;(def neuromancer (load-resource "neuromancer.txt"))
@@ -54,13 +54,13 @@
   [coll]
   (map first coll))
 
-(defn build-books-markov
-  []
-  (->>
-   @combined-books
-   tokenize
-   pos-tag
-   build-from-coll))
+;; (defn build-books-markov
+;;   []
+;;   (->>
+;;    @combined-books
+;;    tokenize
+;;    pos-tag
+;;    build-from-coll))
 
 (defn paragraphs-from
   [corpus]
@@ -73,7 +73,6 @@
 ;; (def markov (build-books-markov))
 
 ;; (detokenize (words-from-pos (take 100 (generate-walk [["Manfred" "NNP"]] markov))))
-
 
 (defn nth-paragraphs
   [corpus num]
@@ -95,8 +94,6 @@
 ;;(def cleaned-neuromancer (cleanup-neuromancer neuromancer))
 ;;(def neuromancer-paragraphs (paragraphs-from cleaned-neuromancer))
 ;;(def accelerando-paragraphs (paragraphs-from accelerando))
-
-
 
 ;;(def markovs-by-para (map (fn [p1 p2] (->> (str p1 "\n" p2) tokenize pos-tag build-from-coll)) accelerando-paragraphs neuromancer-paragraphs))
 
@@ -127,20 +124,17 @@
 ;;     (print (str (fn-name func) " got " (count input) " items."))
 ;;     (func input)))
 
-(defn intercept [input]
-  (println "...")
-  input)
+;; (defn intercept [input]
+;;   (println "...")
+;;   input)
 
 (defn markov-from-books []
   (->
    (str (load-resource "accelerando.txt")
         "\n"
         (cleanup-neuromancer (load-resource "neuromancer.txt")))
- ;;  intercept
    tokenize
- ;;  intercept
    pos-tag
- ;;  intercept
    build-from-coll))
 
 ;; (def markov-parsed (markov-from-books))
@@ -154,7 +148,11 @@
 ;; (def tokenized-corpus (tokenize corpus))
 ;; (def tagged-corpus (pos-tag tokenized-corpus))
 
-(defn generate-paragraphs
-  [n]
-  (let [avg-words 52]
-    (repeatedly n (take (rand avg-words) (generate-walk markov-parsed)))))
+;; (defn generate-paragraphs
+;;   [n]
+;;   (let [avg-words 52]
+;;     (repeatedly n (take (rand avg-words) (generate-walk markov-parsed)))))
+
+;; Calling pos-tag on the entire corpus (2 books) takes too long!
+;; Instead we need an async pos-tag that collects all the tagged data at the end
+
